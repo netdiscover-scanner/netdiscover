63db8216d829d7e41b08279e3fdee9e32d74ddea
jpenalbae <jpenalbae@89c8cec6-37ec-4da5-8e0c-557245be5fec>
2016-04-12 17:34:16 +0000

    Fix for debian bug #818791

--- trunk/src/data_reply.c
+++ trunk/src/data_reply.c
@@ -98,7 +98,7 @@ void reply_print_line()
 
 
    /* Fill again with spaces and cut the string to fit width */
-   for (j=strlen(line); j<win_sz.ws_col - 1; j++)
+   for (j=strlen(line); (j<win_sz.ws_col - 1) && (j<sizeof(line)-1) ; j++)
       strcat(line, blank);
    string_cutter(line, win_sz.ws_col - 1 );
 
