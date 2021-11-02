e1fbdf479c01b542fdc01ec50290bfe2b702d278
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2021-11-01 21:31:15 -0300

    Update Changelog and add rights for bcoles

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
  *  Copyright 2007-2008 Gustavo Chain
  *  Copyright 2009      Janusz Uzycki <j.uzycki@elproma.com.pl>
  *  Copyright 2019-2020 Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
+ *  Copyright 2021      bcoles <bcoles@gmail.com>
  *
  ****************************************************************************/
 
